5ae518ffbeb16f92f8a8c0710607efd8c0739e24
fweathers <fel.weathers@gmail.com>
2015-09-06 14:13:15 -0400

    won't let me redefine windInYourSails

--- objc-parrot/FISAppDelegate.m
+++ objc-parrot/FISAppDelegate.m
@@ -19,11 +19,39 @@
     // Override point for customization after application launch.
     
     NSLog(@"Squawk!");
+    NSLog(@"%@", @"Squawk!");
+    NSLog(@"%@!", @"Squawk");
+    NSLog(@"%@!", [@"Squawk" uppercaseString]);
+    
     NSString *squawk = @"squawk";
     NSLog(@"%@", squawk);
-    squawk = [squawk capitalizedString];
+    squawk = [squawk uppercaseString];
+    NSLog(@"%@", squawk);
     squawk = [squawk stringByAppendingString:@"!"];
     NSLog(@"%@", squawk);
+    
+    NSString *wind = @"Wind";
+    NSString *inString = @"in";
+    NSString *yer = @"yer";
+    NSString *sails = @"sails";
+    NSLog(@"%@ %@ %@ %@!", wind, inString, yer, sails);
+    
+    NSString *windInYerSails = [NSString stringWithFormat:@"%@ %@ %@ %@!", wind, inString, yer, sails];
+    NSLog(@"%@", windInYerSails);
+    
+    NSLog(@"%@", wind);
+    NSString *windInYerSails = [NSString stringWithString:wind];
+    NSLog(@"%@", windInYerSails);
+    
+    windInYerSails = [windInYerSails stringByAppendingString:@" "];
+    windInYerSails = [windInYerSails stringByAppendingString:inString];
+    NSLog(@"%@", windInYerSails);
+    
+    windInYerSails = [windInYerSails stringByAppendingFormat:@"%@", yer];
+    NSLog(@"%@", windInYerSails);
+    windInYerSails = [windInYerSails stringByAppendingFormat:@" %@", sails];
+    NSLog(@"%@", windInYerSails);
+    
     
     NSString *deadMen = @"Dead men";
     NSString *tell = @"tell";
